6894202903e49fa676477650fd34527fd960bd27
Akhil Anwer M <akhilanwarm@gmail.com>
2024-10-08 11:43:22 +0530

    basic project setup backend and front end

---
+++ my-app/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()) //to parse json data from requests
+
+app.get('/api',(req,res)=>{
+  res.send('API is running');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+   .then(() => console.log('MongoDB connected'))
+   .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, ()=>{
+  console.log(`server is running on port ${PORT}`);
+})
+
